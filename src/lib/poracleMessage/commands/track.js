2e9f4dcb067818ac23b0817d5ba044af4c535c81
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2021-08-04 21:31:46 -0400

    Expand Security for track
    - Rename regex and default variables to match sql columns for more convenient iterating
    - Cleanup repeat code

--- src/lib/poracleMessage/commands/track.js
+++ src/lib/poracleMessage/commands/track.js
@@ -27,21 +27,6 @@ exports.run = async (client, msg, args, options) => {
 			return msg.reply(translator.translate('You do not have permission to execute this command'))
 		}
 
-		const securityCheck = [
-			['maxatk', client.re.maxatkRe],
-			['maxdef', client.re.maxdefRe],
-		]
-
-		for (const security of securityCheck) {
-			if (args.some((x) => x.match(security[1]))) {
-				if (!await util.commandAllowed(security[0])) {
-					await msg.react('🚫')
-					return msg.reply(translator.translateFormat('You do not have permission to use the `{0}` parameter',
-						translator.translate(security[0])))
-				}
-			}
-		}
-
 		if (args.length === 0) {
 			await msg.reply(translator.translateFormat('Valid commands are e.g. `{0}track charmander`, `{0}track everything iv100`, `{0}track gible d500`', util.prefix),
 				{ style: 'markdown' })
@@ -61,35 +46,35 @@ exports.run = async (client, msg, args, options) => {
 		let reaction = '👌'
 		// Set defaults
 		let monsters
-		let distance = 0
-		let minTime = 0
-		let cp = 0
-		let maxcp = 9000
-		let iv = -1
-		let maxiv = 100
-		let level = 0
-		let maxlevel = 40
-		let atk = 0
-		let def = 0
-		let sta = 0
-		let maxAtk = 15
-		let maxDef = 15
-		let maxSta = 15
-		let gender = 0
-		let weight = 0
-		let maxweight = 9000000
-		let rarity = -1
-		let maxRarity = 6
-		let greatLeague = 4096
-		let greatLeagueCP = 0
-		let ultraLeague = 4096
-		let ultraLeagueCP = 0
+		const trackDefaults = {
+			distance: 0,
+			min_time: 0,
+			min_cp: 0,
+			max_cp: 9000,
+			min_iv: -1,
+			max_iv: 100,
+			min_level: 0,
+			max_level: 40,
+			atk: 0,
+			def: 0,
+			sta: 0,
+			max_atk: 15,
+			max_def: 15,
+			max_sta: 15,
+			gender: 0,
+			min_weight: 0,
+			max_weight: 9000000,
+			rarity: -1,
+			max_rarity: 6,
+			great_league_ranking: 4096,
+			great_league_ranking_min_cp: 0,
+			ultra_league_ranking: 4096,
+			ultra_league_ranking_min_cp: 0,
+			clean: false,
+			template: client.config.general.defaultTemplateName,
+			ping: msg.getPings(),
+		}
 		const pvpFilterMaxRank = Math.min(client.config.pvp.pvpFilterMaxRank, 4096)
-		const { pvpFilterGreatMinCP } = client.config.pvp
-		const { pvpFilterUltraMinCP } = client.config.pvp
-		let template = client.config.general.defaultTemplateName
-		let clean = false
-		const pings = msg.getPings()
 
 		let disableEverythingTracking
 		let forceEverythingSeparately
@@ -98,26 +83,26 @@ exports.run = async (client, msg, args, options) => {
 			case 'allow-any': {
 				disableEverythingTracking = false
 				forceEverythingSeparately = false
-				individuallyAllowed	= true
+				individuallyAllowed = true
 				break
 			}
 			case 'allow-and-always-individually': {
 				disableEverythingTracking = false
 				forceEverythingSeparately = true
-				individuallyAllowed	= true
+				individuallyAllowed = true
 				break
 			}
 			case 'allow-and-ignore-individually': {
 				disableEverythingTracking = false
 				forceEverythingSeparately = false
-				individuallyAllowed	= false
+				individuallyAllowed = false
 				break
 			}
 			case 'deny':
 			default: {
 				disableEverythingTracking = true
 				forceEverythingSeparately = false
-				individuallyAllowed	= true
+				individuallyAllowed = true
 			}
 		}
 
@@ -137,26 +122,20 @@ exports.run = async (client, msg, args, options) => {
 		const argTypes = args.filter((arg) => typeArray.includes(arg))
 		const genCommand = args.filter((arg) => arg.match(client.re.genRe))
 		const gen = genCommand.length ? client.GameData.utilData.genData[+(genCommand[0].match(client.re.genRe)[2])] : 0
-		if (formNames.length) {
+		if (formNames.length || gen || (args.includes('individually') && (individuallyAllowed || msg.isFromAdmin)) || forceEverythingSeparately) {
 			monsters = Object.values(client.GameData.monsters).filter((mon) => (
 				(args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString()))
-				|| mon.types.map((t) => t.name.toLowerCase()).find((t) => argTypes.includes(t))
-				|| args.includes('everything') && !disableEverythingTracking
-				|| args.includes('everything') && msg.isFromAdmin) && formNames.includes(mon.form.name.toLowerCase()))
+        || mon.types.map((t) => t.name.toLowerCase()).find((t) => argTypes.includes(t))
+        || args.includes('everything') && !disableEverythingTracking
+        || args.includes('everything') && msg.isFromAdmin) && !mon.form.id)
 
-			if (gen) monsters = monsters.filter((mon) => mon.id >= gen.min && mon.id <= gen.max)
-		} else if (gen || (args.includes('individually') && (individuallyAllowed || msg.isFromAdmin)) || forceEverythingSeparately) {
-			monsters = Object.values(client.GameData.monsters).filter((mon) => (
-				(args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString()))
-				|| mon.types.map((t) => t.name.toLowerCase()).find((t) => argTypes.includes(t))
-				|| args.includes('everything') && !disableEverythingTracking
-				|| args.includes('everything') && msg.isFromAdmin) && !mon.form.id)
-
-			if (gen) monsters = monsters.filter((mon) => mon.id >= gen.min && mon.id <= gen.max)
+			if (gen) {
+				monsters = monsters.filter((mon) => mon.id >= gen.min && mon.id <= gen.max)
+			}
 		} else {
 			monsters = Object.values(client.GameData.monsters).filter((mon) => (
 				(args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString()))
-				|| mon.types.map((t) => t.name.toLowerCase()).find((t) => argTypes.includes(t))) && !mon.form.id)
+        || mon.types.map((t) => t.name.toLowerCase()).find((t) => argTypes.includes(t))) && !mon.form.id)
 
 			if (args.includes('everything') && !disableEverythingTracking || args.includes('everything') && msg.isFromAdmin) {
 				monsters.push({
@@ -169,117 +148,97 @@ exports.run = async (client, msg, args, options) => {
 		}
 		// Parse command elements to stuff
 		for (const element of args) {
-			if (element.match(client.re.maxlevelRe)) [,, maxlevel] = element.match(client.re.maxlevelRe)
-			else if (element.match(client.re.templateRe)) [,, template] = element.match(client.re.templateRe)
-			else if (element.match(client.re.greatLeagueRe)) [,, greatLeague] = element.match(client.re.greatLeagueRe)
-			else if (element.match(client.re.greatLeagueCPRe)) [,, greatLeagueCP] = element.match(client.re.greatLeagueCPRe)
-			else if (element.match(client.re.ultraLeagueRe)) [,, ultraLeague] = element.match(client.re.ultraLeagueRe)
-			else if (element.match(client.re.ultraLeagueCPRe)) [,, ultraLeagueCP] = element.match(client.re.ultraLeagueCPRe)
-			else if (element.match(client.re.maxcpRe)) [,, maxcp] = element.match(client.re.maxcpRe)
-			else if (element.match(client.re.maxivRe)) [,, maxiv] = element.match(client.re.maxivRe)
-			else if (element.match(client.re.maxweightRe)) [,, maxweight] = element.match(client.re.maxweightRe)
-			else if (element.match(client.re.maxRarityRe)) [,, maxRarity] = element.match(client.re.maxRarityRe)
-			else if (element.match(client.re.maxatkRe)) [,, maxAtk] = element.match(client.re.maxatkRe)
-			else if (element.match(client.re.maxdefRe)) [,, maxDef] = element.match(client.re.maxdefRe)
-			else if (element.match(client.re.maxstaRe)) [,, maxSta] = element.match(client.re.maxstaRe)
-			else if (element.match(client.re.cpRe)) [,, cp] = element.match(client.re.cpRe)
-			else if (element.match(client.re.levelRe)) [,, level] = element.match(client.re.levelRe)
-			else if (element.match(client.re.ivRe)) [,, iv] = element.match(client.re.ivRe)
-			else if (element.match(client.re.atkRe)) [,, atk] = element.match(client.re.atkRe)
-			else if (element.match(client.re.defRe)) [,, def] = element.match(client.re.defRe)
-			else if (element.match(client.re.staRe)) [,, sta] = element.match(client.re.staRe)
-			else if (element.match(client.re.weightRe)) [,, weight] = element.match(client.re.weightRe)
-			else if (element.match(client.re.tRe)) [,, minTime] = element.match(client.re.tRe)
-			else if (element.match(client.re.rarityRe)) [,, rarity] = element.match(client.re.rarityRe)
-			else if (element.match(client.re.dRe)) [,, distance] = element.match(client.re.dRe)
-			else if (element === 'female') gender = 2
-			else if (element === 'clean') clean = true
-			else if (element === 'male') gender = 1
-			else if (element === 'genderless') gender = 3
+			let match
+			let input = element
+			switch (element) {
+				case 'male': trackDefaults.gender = 1; break
+				case 'female': trackDefaults.gender = 2; break
+				case 'genderless': trackDefaults.gender = 3; break
+				case 'clean': trackDefaults.clean = true; break
+				default:
+					match = Object.keys(client.re).find((x) => element.match(client.re[x]))
+					if (match) {
+						const command = match.substring(0, match.length - 2)
+						input = command.includes('ranking') ? command.split('_')[0] : command.replace('_', '')
+						if (command) [, , trackDefaults[command]] = element.match(client.re[match])
+					}
+			}
+			if (!await util.commandAllowed(input)) {
+				await msg.react('🚫')
+				return msg.reply(translator.translateFormat('You do not have permission to use the `{0}` parameter',
+					translator.translate(input)))
+			}
 		}
-		if (greatLeague < 4096 && ultraLeague < 4096 || greatLeague < 4096 && ultraLeagueCP > 0 || greatLeagueCP > 0 && ultraLeague < 4096 || greatLeagueCP > 0 && ultraLeagueCP > 0) {
+		if ((trackDefaults.great_league_ranking < 4096 && trackDefaults.ultra_league_ranking < 4096) || (trackDefaults.great_league_ranking < 4096 && trackDefaults.ultra_league_ranking_min_cp > 0) || (trackDefaults.great_league_ranking_min_cp > 0 && trackDefaults.ultra_league_ranking < 4096) || (trackDefaults.great_league_ranking_min_cp > 0 && trackDefaults.ultra_league_ranking_min_cp > 0)) {
 			await msg.react(translator.translate('🙅'))
 			return await msg.reply(`${translator.translate('Oops, both Great and Ultra league parameters were set in command! - check the')} \`${util.prefix}${translator.translate('help')}\``)
 		}
 
-		// if a value for great/ultra league rank was given, force it to be not greater than pvpFilterMaxRank
-		if (greatLeague < 4096 && greatLeague > pvpFilterMaxRank) greatLeague = pvpFilterMaxRank
-		if (ultraLeague < 4096 && ultraLeague > pvpFilterMaxRank) ultraLeague = pvpFilterMaxRank
-		// if a value for great/ultra league CP was given, force it to be not less than pvpFilterGreatMinCP/pvpFilterUltraMinCP
-		if (greatLeagueCP > 0 && greatLeagueCP < pvpFilterGreatMinCP) greatLeagueCP = pvpFilterGreatMinCP
-		if (ultraLeagueCP > 0 && ultraLeagueCP < pvpFilterUltraMinCP) ultraLeagueCP = pvpFilterUltraMinCP
-		// if a value for great/ultra league rank was given but none for great/ultra league CP, set the later implicitly to pvpFilterGreatMinCP/pvpFilterUltraMinCP
-		if (greatLeague < 4096 && greatLeagueCP === 0) greatLeagueCP = pvpFilterGreatMinCP
-		if (ultraLeague < 4096 && ultraLeagueCP === 0) ultraLeagueCP = pvpFilterUltraMinCP
-		// if a value for great/ultra league CP was given but none for great/ultra league rank, set the later implicitly to pvpFilterMaxRank
-		if (greatLeagueCP > 0 && greatLeague === 4096) greatLeague = pvpFilterMaxRank
-		if (ultraLeagueCP > 0 && ultraLeague === 4096) ultraLeague = pvpFilterMaxRank
+		['great_league_ranking', 'ultra_league_ranking'].forEach((league, i) => {
+			const minCp = `${league}_min_cp`
+			const minCpFilter = i ? 'pvpFilterUltraMinCP' : 'pvpFilterGreatMinCP'
+			// if a value for great/ultra league rank was given, force it to be not greater than pvpFilterMaxRank
+			if (trackDefaults[league] < 4096 && trackDefaults[league] > pvpFilterMaxRank) {
+				trackDefaults[league] = pvpFilterMaxRank
+			}
+			// if a value for great/ultra league CP was given, force it to be not less than pvpFilterGreatMinCP/pvpFilterUltraMinCP
+			if (trackDefaults[minCp] > 0 && trackDefaults[minCp] < client.config.pvp[minCpFilter]) {
+				trackDefaults[minCp] = client.config.pvp[minCpFilter]
+			}
+			// if a value for great/ultra league rank was given but none for great/ultra league CP, set the later implicitly to pvpFilterGreatMinCP/pvpFilterUltraMinCP
+			if (trackDefaults[league] < 4096 && trackDefaults[minCp] === 0) {
+				trackDefaults[minCp] = client.config.pvp[minCpFilter]
+			}
+			// if a value for great/ultra league CP was given but none for great/ultra league rank, set the later implicitly to pvpFilterMaxRank
+			if (trackDefaults[minCp] > 0 && trackDefaults[league] === 4096) {
+				trackDefaults[league] = pvpFilterMaxRank
+			}
+		})
 
-		if (client.config.tracking.defaultDistance !== 0 && distance === 0 && !msg.isFromAdmin) distance = client.config.tracking.defaultDistance
-		if (client.config.tracking.maxDistance !== 0 && distance > client.config.tracking.maxDistance && !msg.isFromAdmin) distance = client.config.tracking.maxDistance
+		if (client.config.tracking.defaultDistance !== 0 && trackDefaults.distance === 0 && !msg.isFromAdmin) {
+			trackDefaults.distance = client.config.tracking.defaultDistance
+		}
+		if (client.config.tracking.maxDistance !== 0 && trackDefaults.distance > client.config.tracking.maxDistance && !msg.isFromAdmin) {
+			trackDefaults.distance = client.config.tracking.maxDistance
+		}
 
-		if (rarity !== -1 && !['1', '2', '3', '4', '5', '6'].includes(rarity)) {
-			rarity = client.translatorFactory.reverseTranslateCommand(rarity, true)
-			const rarityLevel = Object.keys(client.GameData.utilData.rarity).find((x) => client.GameData.utilData.rarity[x].toLowerCase() === rarity.toLowerCase())
+		if (trackDefaults.rarity !== -1 && !['1', '2', '3', '4', '5', '6'].includes(trackDefaults.rarity)) {
+			trackDefaults.rarity = client.translatorFactory.reverseTranslateCommand(trackDefaults.rarity, true)
+			const rarityLevel = Object.keys(client.GameData.utilData.rarity).find((x) => client.GameData.utilData.rarity[x].toLowerCase() === trackDefaults.rarity.toLowerCase())
 			if (rarityLevel) {
-				rarity = rarityLevel
+				trackDefaults.rarity = rarityLevel
 			} else {
-				rarity = -1
+				trackDefaults.rarity = -1
 			}
 		}
-		if (maxRarity !== 6 && !['1', '2', '3', '4', '5', '6'].includes(maxRarity)) {
-			maxRarity = client.translatorFactory.reverseTranslateCommand(maxRarity, true)
-			const maxRarityLevel = Object.keys(client.GameData.utilData.rarity).find((x) => client.GameData.utilData.rarity[x].toLowerCase() === maxRarity.toLowerCase())
+		if (trackDefaults.max_rarity !== 6 && !['1', '2', '3', '4', '5', '6'].includes(trackDefaults.max_rarity)) {
+			trackDefaults.max_rarity = client.translatorFactory.reverseTranslateCommand(trackDefaults.max_rarity, true)
+			const maxRarityLevel = Object.keys(client.GameData.utilData.rarity).find((x) => client.GameData.utilData.rarity[x].toLowerCase() === trackDefaults.max_rarity.toLowerCase())
 			if (maxRarityLevel) {
-				maxRarity = maxRarityLevel
+				trackDefaults.max_rarity = maxRarityLevel
 			} else {
-				maxRarity = 6
+				trackDefaults.max_rarity = 6
 			}
 		}
 
-		if (distance > 0 && !userHasLocation && !target.webhook) {
+		if (trackDefaults.distance > 0 && !userHasLocation && !target.webhook) {
 			await msg.react(translator.translate('🙅'))
 			return await msg.reply(`${translator.translate('Oops, a distance was set in command but no location is defined for your tracking - check the')} \`${util.prefix}${translator.translate('help')}\``)
 		}
-		if (distance === 0 && !userHasArea && !target.webhook && !msg.isFromAdmin) {
+		if (trackDefaults.distance === 0 && !userHasArea && !target.webhook && !msg.isFromAdmin) {
 			await msg.react(translator.translate('🙅'))
 			return await msg.reply(`${translator.translate('Oops, no distance was set in command and no area is defined for your tracking - check the')} \`${util.prefix}${translator.translate('help')}\``)
 		}
-		if (distance === 0 && !userHasArea && !target.webhook && msg.isFromAdmin) {
+		if (trackDefaults.distance === 0 && !userHasArea && !target.webhook && msg.isFromAdmin) {
 			await msg.reply(`${translator.translate('Warning: Admin command detected without distance set - using default distance')} ${client.config.tracking.defaultDistance}`)
-			distance = client.config.tracking.defaultDistance
+			trackDefaults.distance = client.config.tracking.defaultDistance
 		}
 		const insert = monsters.map((mon) => ({
+			...trackDefaults,
 			id: target.id,
 			profile_no: currentProfileNo,
 			pokemon_id: mon.id,
-			ping: pings,
-			distance: +distance,
-			min_iv: +iv,
-			max_iv: +maxiv,
-			min_cp: +cp,
-			max_cp: +maxcp,
-			min_level: +level,
-			max_level: +maxlevel,
-			atk: +atk,
-			def: +def,
-			sta: +sta,
-			template: template.toString(),
-			min_weight: +weight,
-			max_weight: +maxweight,
 			form: mon.form.id,
-			max_atk: +maxAtk,
-			max_def: +maxDef,
-			max_sta: +maxSta,
-			gender: +gender,
-			clean: +clean,
-			great_league_ranking: +greatLeague,
-			great_league_ranking_min_cp: +greatLeagueCP,
-			ultra_league_ranking: +ultraLeague,
-			ultra_league_ranking_min_cp: +ultraLeagueCP,
-			rarity: +rarity,
-			max_rarity: +maxRarity,
-			min_time: +minTime,
 		}))
 		if (!insert.length) {
 			return await msg.reply(translator.translate('404 No monsters found'))
